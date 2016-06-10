b9b98778df472ef6fe21f3401ac3370e0a2b757d
mike <illlust@gmail.com>
2016-06-10 16:44:36 +0900

    deploy

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello, heroku');
+  res.send('Hello, Heroku 2');
 });
 
 app.listen(process.env.PORT || 8080, function() {
