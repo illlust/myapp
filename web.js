f4ab6a03d7402ff7cd1db9e863d390367100906d
mike <illlust@gmail.com>
2016-06-10 17:08:57 +0900

    sb

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello, Heroku 3');
+  res.send('Hello, heroku on sublime text');
 });
 
 app.listen(process.env.PORT || 8080, function() {
